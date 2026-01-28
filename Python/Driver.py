e67a25fadd0aa7a01e70640b85cbadbf28674128
Alan Briseno <alanbriseno50@gmail.com>
2026-01-28 00:07:53 -0600

    Refactor Driver.py with improved comments and settings
    Updated comments for clarity and adjusted simulation parameters for accuracy.

--- Python/Driver.py
+++ Python/Driver.py
@@ -1,6 +1,7 @@
-n_moons = int(input("number of moons: "))
-moon_mass = input(" moon mass (L, C, P): ")
+n_moons = int(input("number of moons: ")) #The number of moons you want, e.g. 1, 50, 100
+moon_mass = input(" moon mass (L, C, P): ") #Moon mass which is defined in the constant section
 
+#File writing function
 def write_to_file(fname,f_str,head):
     lock.acquire()
     if head == "head":
@@ -10,25 +11,29 @@ def write_to_file(fname,f_str,head):
     f.write(f_str)
     lock.release()
 
+#Cartesion vectors to keplerian orbital elements
 def Cart2Orb(ps0,ps1,mcen):
   ps_diff = ps1 - ps0
   tempsim = rebound.Simulation()
-  tempsim.units = ('AU', 'yr', 'Msun')
+  tempsim.units = ('AU', 'yr', 'Msun') # Units defined for system
   tempsim.add(m=mcen)
   tempsim.add(m=0,x=ps_diff.x,y=ps_diff.y,z=ps_diff.z,vx=ps_diff.vx,vy=ps_diff.vy,vz=ps_diff.vz)
-  tempsim.move_to_com()
+  tempsim.move_to_com() 
   ps = tempsim.particles
   return ps[1].a,ps[1].e,ps[1].d
 
+#Semi-major (a) function
 def get_semi(delta_H,a_j,k,j):
   X = 0.5*((2*M_moon)/(3*(earth_mass+k*M_moon)))**(1./3.)
   return a_j*((1+delta_H*X)/(1-delta_H*X))**(k-j)
 
+#Orbtial phase function
 def get_TrueAnomaly(j):
   phi = (1+np.sqrt(5))/2.
   golden_angle =  phi * 2 * np.pi
   return (j * golden_angle) % (2 * np.pi)
 
+#Collision function 
 collision_happened = False
 def collision_callback(sim_pointer, collision):
     global collision_happened
@@ -48,9 +53,9 @@ def run_simulation(d_idx):
   delta_H = delta_rng[d_idx]
   sim = rebound.Simulation()
   sim.collision = "direct"
-  sim.collision_resolve = "halt"
+  sim.collision_resolve = "halt" # Collision factor
   sim.units = ('AU', 'yr', 'Msun')
-  sim.integrator = "ias15"
+  sim.integrator = "ias15" 
   sim.ri_trace.peri_mode = 1
   am = np.array([a_m] + [get_semi(delta_H, a_m, i, 0) for i in range(1, n_moons)])
   sim.add(m=star_mass,hash='Sun') # 0
@@ -63,27 +68,27 @@ def run_simulation(d_idx):
   ts = rebx.load_force("tides_spin")
   rebx.add_force(ts)
   ps = sim.particles
-  ps["Earth"].params['k2'] = k2_earth
+  ps["Earth"].params['k2'] = k2_earth / 2
   ps["Earth"].params['I'] = c_earth * earth_mass * r_earth**2 # if m=0, the effect interprets 'I' as I/m, so we could set ...['I'] = c_earth * r_earth**2
   spin_period_earth = (6/24. / 365.25)  # 6 hour spin period
   spin_mag_earth = 2 * np.pi / spin_period_earth
   obl_earth = np.radians(23.44)
   ps["Earth"].params['Omega'] = [0., spin_mag_earth * np.sin(obl_earth), spin_mag_earth * np.cos(obl_earth)]
-  ps["Earth"].params['tau'] = 100./3600./24./365.25
+  ps["Earth"].params['tau'] = 698./3600./24./365.25
   Ltot = np.array(sim.angular_momentum()) + np.array(rebx.spin_angular_momentum())
   rot = rebound.Rotation.to_new_axes(newz=Ltot) # Alignment to invariant plane
   rebx.rotate_simulation(rot)
   rebx.initialize_spin_ode(ts)
 
   Pmoon = np.sqrt(am[0]**3/earth_mass) # Kepler 3rd Law
-  max_time = 1e7*Pmoon # Total spins
+  max_time = 1e5*Pmoon # Total spins
   sim.dt = Pmoon / 20  # Timestep calc per orbit
   sim.ri_ias15.min_dt = Pmoon / 50
   stable = True
-  out_step = 1000*Pmoon # check every nth orbits
+  out_step = 100*Pmoon # check every nth orbits
   emax = np.zeros(n_moons)
   t = sim.t
-  sim.save_to_file("archive.bin", interval=100.)
+  sim.save_to_file("archive3.5.bin", interval=Pmoon)
   try:
     while stable:
       sim.integrate(t)
@@ -91,7 +96,7 @@ def run_simulation(d_idx):
       if t >= max_time:
           stable = False
       for i in range(0,n_moons):
-        moon_semi, moon_ecc, moon_d = Cart2Orb(ps['Earth'],ps[2+i],earth_mass+M_moon) # returns/inputs 
+        moon_semi, moon_ecc, moon_d = Cart2Orb(ps['Earth'],ps[2+i],earth_mass+M_moon) # returns/inputs
         fname = "evolution%i.txt" % i
         with open(fname, "a") as outfile:
           outstg = "%1.2f, %1.4f, %1.4f, %1.4f\n" % (sim.t, moon_semi/R_H, moon_ecc, moon_d/R_H)
@@ -121,7 +126,7 @@ phi = (1+np.sqrt(5))/2.
 star_mass = 1 # Sun M
 earth_mass = 3.003e-6 #Solar M
 earth_semi = 1 #A_M
-earth_ecc = 0.01671022 # Orbit Circulation
+earth_ecc = 0.01671022 # Orbit ecc
 earth_radius = 6371e3/astronomical_unit
 e_m = 1e-7
 r_earth = 4.259e-5
@@ -160,8 +165,9 @@ for i in range(0, n_moons):
   with open(fname, "w") as outfile:
     outfile.write("#Time, Semimajor, Ecc, Distance\n")
     outfile.close()
-delta_rng = np.arange(8.59,8.60,0.1)
-# delta_rng = np.concatenate((np.arange(3.5,4.5,0.01), np.arange(6.9,7.2,0.01)))
+delta_rng = np.arange(4.1, 4.11, 0.1)
+# delta_rng = np.array([3.5])
+# delta_rng = np.concatenate((np.arange(3.5,4.5,0.01), np.arange(7.01,7.2,0.01)))
 # for d in range(0,len(delta_rng)):
 #   run_simulation(d)
 pool = mp.Pool(processes=8)
